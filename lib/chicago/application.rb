fbe98bfbcbbcaa3f5db22fd8de2e73ac6f567598
Justin Knowlden <jaknowlden@gmail.com>
2009-03-12 20:41:20 -0500

    catch_all_css will now pass if SASS file exists

--- lib/chicago/application.rb
+++ lib/chicago/application.rb
@@ -3,17 +3,22 @@ module Sinatra
     module Base
 
       # Assumes all CSS is SASS and is referenced as being in a directory named stylesheets
+      # If SASS file is not defined, the route will passed on to - theoretically - the real
+      # CSS in the public directory.
       def catch_all_css(path='/stylesheets')
         get("#{path}/*.css") do
-          content_type 'text/css'
-          sass params["splat"].first.to_sym
+          begin
+            content_type 'text/css'
+            sass params["splat"].first.to_sym
+          rescue Errno::ENOENT
+            pass
+          end
         end
       end
 
-      # When you don't want anything special, but to load a named view
+      # When you don't want anything special, but to load a named view as HAML.
       def get_obvious(name)
         get "/#{name}" do
-          title = name.to_s
           haml name.to_sym
         end
       end
--- test/application_test.rb
+++ test/application_test.rb
@@ -34,6 +34,16 @@ class ApplicationTest < Test::Unit::TestCase
       should_have_content_type 'text/css'
       should_have_response_body %r[.car \{\s+display: some; \}\s]
     end
+
+    context "with path that's not a defined a sass file" do
+      setup do
+        mock_app { catch_all_css }
+        get '/stylesheets/goo.css'
+      end
+
+      should_have_response_status 404
+      should_have_content_type 'text/css'
+    end
   end # catching all css
 
   context "getting obvious views" do
