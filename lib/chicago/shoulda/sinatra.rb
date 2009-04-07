c4903a86166907c9acc2e6570da2756e1326a34d
Justin Knowlden <jaknowlden@gmail.com>
2009-04-07 18:12:55 -0500

    Added should_be_redirected_to. Bumped gem version

--- lib/chicago/shoulda/sinatra.rb
+++ lib/chicago/shoulda/sinatra.rb
@@ -32,6 +32,18 @@ module ThumbleMonks
             assert_response_body json
           end
         end
+
+        # Checks to see if there is a redirect in play and the path the redirect goes to
+        #
+        # Example:
+        #   should_be_redirected_to { "/users/@user.id" }
+        #   should_be_redirected_to "/home"
+        def self.should_be_redirected_to(expected_path=nil, &block)
+          should "be redirected to #{expected_path | 'a url'}" do
+            expected_path = self.instance_eval(&block) if block_given?
+            assert_redirected_to expected_path
+          end
+        end
       end # Macros
 
       module Helpers
